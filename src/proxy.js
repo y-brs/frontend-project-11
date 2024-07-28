19940bd9e56128791fb3476090e38d371f336524
Boris Yarmulnik <boris.yarmoulnik@gmail.com>
2024-07-29 01:10:49 +0300

    fix: response status

--- src/proxy.js
+++ src/proxy.js
@@ -6,7 +6,7 @@ export default (url) =>
   axios
     .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
     .then((response) => {
-      if (response.status >= 200 && response.status < 300) return response;
+      if (response.status >= 200 && response.status < 400) return response;
       throw new Error(axios.isAxiosError);
     })
     .catch((error) => {
